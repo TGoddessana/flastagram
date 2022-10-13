7b57eb48e0fc13b6cc20f1943038c9578f890956
ingstor <ingstor1092@gmail.com>
2022-10-13 22:36:45 +0900

    테스트 유저 생성 방식 변경

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,7 +1,7 @@
 import unittest
 from api.db import db
 import api
-
+import json
 from dotenv import load_dotenv
 from api.models.post import PostModel
 from api.models.user import UserModel
@@ -18,7 +18,7 @@ class CommonTestCaseSettings(unittest.TestCase):
         backend/config/test.py 를 사용
         .env 파일의 APPLICATION_SETTINGS_FOR_TEST 환경 변수 사용
         app.test_client() 로 테스트를 위한 클라이언트 생성
-        테스트를 위한 임의의 유저 한 명 생성
+        테스트를 위한 임의의 유저 한 명 생성 (회원가입)
         """
         self.app = api.create_app()
         self.ctx = self.app.app_context()
@@ -29,7 +29,20 @@ class CommonTestCaseSettings(unittest.TestCase):
         self.app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
         self.client = self.app.test_client()
         db.create_all()
-        UserModel(username="test_user", password="12345", email="test@example.com").save_to_db()
+
+        # 테스트를 위한 유저 생성 (회원가입)
+        data = {
+            "email": "test@example.com",
+            "username": "test_user",
+            "password": "12345",
+            "password_confirm": "12345",
+        }
+        # 회원가입 진행
+        self.client.post(
+            "http://127.0.0.1:5000/register/",
+            data=json.dumps(data),
+            content_type="application/json",
+        )
 
     def tearDown(self):
         """

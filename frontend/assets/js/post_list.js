8cd78fb880c25d61c28af0fd8fe8dfefacfbdc06
ingstor <ingstor1092@gmail.com>
2022-11-13 03:06:40 +0900

    무한 스크롤 구현

--- frontend/assets/js/post_list.js
+++ frontend/assets/js/post_list.js
@@ -21,7 +21,7 @@ async function getPostListDatafromAPI(page = 1) {
 function copyDiv() {
   const postDiv = document.querySelector(".post");
   const newNode = postDiv.cloneNode(true);
-  newNode.id = "copied-posts";
+  newNode.id = "copied-post";
   postDiv.after(newNode);
 }
 
@@ -29,8 +29,8 @@ function copyDiv() {
  * getPostListDatafromAPI() 로부터 게시물 목록 데이터를 불러옵니다.
  * 불러온 데이터 결과의 길이만큼 (페이지네이션 처리) 게시물을 반복해 그립니다.
  */
-function loadPosts() {
-  getPostListDatafromAPI()
+function loadPosts(page = 1) {
+  getPostListDatafromAPI((page = page))
     .then((result) => {
       for (let i = 0; i < result.length; i++) {
         copyDiv();
@@ -53,7 +53,7 @@ function loadPosts() {
         contentElement.innerText = result[result.length - 1 - i]["content"];
         // 게시물이 없다면 none 처리를 합니다.
         if (i == 0) {
-          document.getElementById("copied-posts").style.display = "none";
+          document.getElementById("copied-post").style.display = "none";
         }
       }
     })
@@ -62,4 +62,88 @@ function loadPosts() {
     });
 }
 
-loadPosts(); // 최종 함수 호출
+/**
+ * post Div 전체를 복사해 반환합니다.
+ */
+function getCopyDiv() {
+  const postDiv = document.querySelector(".post");
+  const newNode = postDiv.cloneNode(true);
+  newNode.id = "copied-post";
+  return newNode;
+}
+
+/**
+ * 제목, 내용, 저자, 사진을 받아 해당 div를 하나의 게시물로 완성합니다.
+ */
+function getCompletedPost(
+  titleValue,
+  contentValue,
+  authorNameValue,
+  feedImgValue
+) {
+  div = getCopyDiv();
+  let authorUpImg = div.children[0].children[0].children[0];
+  let authorUpName = div.children[0].children[0].children[1];
+  let feedImg = div.children[1];
+  let authorDownName = div.children[2].children[3];
+  let title = div.children[2].children[4];
+  let content = div.children[2].children[5];
+  let postTime = div.children[2].children[6];
+
+  title.innerText = titleValue;
+  content.innerText = contentValue;
+  authorUpName.innerText = authorNameValue;
+  authorDownName.innerText = authorNameValue;
+  feedImg.src = feedImgValue;
+
+  return div;
+}
+
+/**
+ * 게시물 데이터를 받아온 다음,
+ * 일정한 조건이 되면 호출되는 메서드입니다.
+ * 페이지를 받아서, 적절한 데이터를 받아 화면에 그립니다.
+ */
+function loadMorePosts(page) {
+  getPostListDatafromAPI(page).then((result) => {
+    const postDiv = document.querySelector(".post-wrapper");
+    for (let i = 0; i < result.length; i++) {
+      const title = result[i]["title"];
+      const content = result[i]["content"];
+      const author = result[i]["author_name"];
+      const image = imageRetrieveBseUrl + result[i]["image"];
+
+      postDiv.append(
+        getCompletedPost(
+          (titleValue = title),
+          (contentValue = content),
+          (authorNameValue = author),
+          (feedImgValue = image)
+        )
+      );
+    }
+  });
+}
+
+/**
+ * 무한 스크롤을 수행합니다.
+ */
+function executeInfiniteScroll() {
+  let pageCount = 2; // 먼저 페이지 수를 2페이지부터 로드한다고 정해두고,
+  var intersectionObserver = new IntersectionObserver(function (entries) {
+    if (entries[0].intersectionRatio <= 0) {
+      return;
+    }
+    // 게시물을 더 로드합니다.
+    loadMorePosts(pageCount);
+    pageCount++;
+  });
+  intersectionObserver.observe(document.querySelector(".bottom"));
+}
+
+function main() {
+  loadPosts(1); // 먼저 첫 페이지 게시물들 다 보여주고,
+  executeInfiniteScroll(); // 스크롤을 내릴 때마다 게시물을 로드 (무한스크롤)
+}
+
+main();
